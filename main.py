6558b0e443a71a7f9581b2e24a33c151ccce2de7
Yashvi <yashvi.sharma1204@gmail.com>
2024-06-09 00:31:32 +0530

    Update the to do app requirements

--- main.py
+++ main.py
@@ -1,35 +1,8 @@
-def get_todos():
-    """
-    Read a text file and return the list of to-do items.
-    """
-    with open('todos.txt', 'r') as file:
-        todos = file.readlines()
-    return todos
+from functions import get_todos,append_todos,write_todo
+import time
 
-def append_todos(todo):
-    """
-    Append a new to-do item to the end of the file.
-
-    Parameters:
-        todo (str): The new to-do item to be appended.
-    
-    Returns:
-        str: The added to-do item.
-    """
-    with open('todos.txt', 'a') as file:
-        file.write(todo)
-    return todo
-
-def write_todo(todo):
-    """
-    Write a new to-do item to the file, replacing its contents.
-
-    Parameters:
-        todo (str): The new to-do item to be written.
-    """
-    with open('todos.txt', 'w') as file:
-        file.writelines(todo)
-
+now = time.strftime("%b %d,%Y %H:%M:%S")
+print("It is,",now)
 while True:
     user_action = input("Type add, show, edit, complete, or exit: ").strip()
     
